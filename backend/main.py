b44dabd48c914188c1aa5ea0e007e29f9a4f471f
rhron255 <ronhasleton@gmail.com>
2024-10-17 14:54:09 +0300

    Made api use function names.

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,12 @@
 import uuid
-
 import uvicorn
-from fastapi import FastAPI, Response
-from starlette.websockets import WebSocket
-
 from UserManager import UserManager
 from dto.RoomDto import RoomDto
+from fastapi import FastAPI, Response
+from fastapi.routing import APIRoute
 from models.Room import Room
+from starlette.staticfiles import StaticFiles
+from starlette.websockets import WebSocket
 
 app = FastAPI(servers=[{"url": "/api"}])
 
@@ -14,6 +14,17 @@ users = UserManager("./users.json")
 rooms: dict[str, Room] = {}
 counter = 0
 shapes = {}
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+def init_rooms():
+    for i in range(20):
+        new_room = Room(f"Test room {i}")
+        rooms[new_room.id] = new_room
+
+
+init_rooms()
 
 
 @app.get("/login", tags=["login"])
@@ -47,8 +58,6 @@ async def websocket_endpoint(room_id: str, user_id: str, websocket: WebSocket):
         current_room = rooms[room_id]
         current_room.join(user_id, websocket)
         while True:
-            print(f"Sending data from {user_id}")
-            print("AAAAAAAAAAAAAAAAAAAAAAAAAAA")
             if user_id not in shapes:
                 user_shape = 'X' if counter % 2 == 0 else 'O'
                 shapes[user_id] = user_shape
@@ -56,14 +65,24 @@ async def websocket_endpoint(room_id: str, user_id: str, websocket: WebSocket):
                 user_shape = shapes[user_id]
             await websocket.send_json({"board": current_room.board, "shape": user_shape})
             counter += 1
-            print(f"Receiving data from {user_id}")
             json_ = await websocket.receive_json()
-            print(json_)
             current_room.board = json_
             await current_room.broadcast_state()
-            # if current_room.current_player.id == user_id:
-            #     current_room.pass_turn()
 
+
+def use_route_names_as_operation_ids(fast_api_app: FastAPI) -> None:
+    """
+    Simplify operation IDs so that generated API clients have simpler function
+    names.
+
+    Should be called only after all routes have been added.
+    """
+    for route in fast_api_app.routes:
+        if isinstance(route, APIRoute):
+            route.operation_id = route.name  # in this case, 'read_items'
+
+
+use_route_names_as_operation_ids(app)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8001)
